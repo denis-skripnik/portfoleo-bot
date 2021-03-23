016133ad4682cc5f1515c8cc30e1f4ad86fc4a79
denis-skripnik <scadens@yandex.ru>
2021-03-23 17:43:06 +0700

    Update data

--- telegrambot.php
+++ telegrambot.php
@@ -12,7 +12,7 @@ if ($text && $text === '/start') {
     $msg = $lng['ru']['home_text'];
     $prices = getPage('https://api.coingecko.com/api/v3/simple/price?ids=BITCOIN%2CETHEREUM%2CCOSMOS%2CPOLKADOT%2CBIP%2CNEAR%2CSTEEM%2CTRON%2CGOLOS&vs_currencies=USD', 8);
     $viz_price = getPage('https://eiy5lsown5.execute-api.us-east-2.amazonaws.com/', 9);
-    $prices['viz'] = ['usd' => $viz_price['average_bid_price']];
+    $prices['viz'] = ['usd' => (float)$viz_price['average_bid_price']];
     foreach ($prices as $name => $price) {
         $data = getBalances($name);
         if (isset($data['balance']) && $data['balance'] === 0) continue;
@@ -22,8 +22,10 @@ $balance = '';
     $coin = '<a href="'.$data['link'].'">'.$name.'</a>';
 }
 if (isset($data['balance'])) $balance = ' ('.$lng['ru']['balance'].' '.round($data['balance'], 3).')';
+        $usd_price = round($price['usd'], 2);
+        if ($price['usd'] < 0.01) $usd_price = round($price['usd'], 7);
         $msg .= '
-'.$coin.' - '.round($price['usd'], 6).$balance;
+'.$coin.' - '.$usd_price.$balance;
     }
     $arInfo["inline_keyboard"] = [[['url' => 'https://t.me/blind_dev_contact_bot', 'text' => $lng['ru']['contact']]]];
 // $arInfo["keyboard"][0][0]["text"] = "Кнопка";    
@@ -45,7 +47,7 @@ $chat_id = $arr['message']['chat']['id'];
 if (!isset($arr['callback_query'])) {
     $text = (isset($arr['message']['text']) ? $arr['message']['text'] : '');
     $data = botControl($text);
-$tg->send($chat_id, $data['msg'], 0, $data['buttons']);
+$tg->photo($chat_id, $data['msg'], 0, $data['buttons']);
 } else if (isset($arr['callback_query']) && $arr['callback_query']['data']) {
     $data = botControl($arr['callback_query']['data']);
     $tg->edit($arr['callback_query']['message']['chat']['id'], $arr['callback_query']['message']['message_id'], $data['msg'], $data['buttons']);
--- telegramgclass.php
+++ telegramgclass.php
@@ -1,149 +1,183 @@
 <?php
-class TG {
-  
+
+class TG
+{
     public $token = '';
-  
-    public function __construct($token) {
-        $this->token = $token; 
+
+    public function __construct($token)
+    {
+        $this->token = $token;
     }
-      
-    public function send($id, $message,$reply_to_message_id,$keyboard) {   
-		
-		//Удаление клавы
-		if($keyboard == "DEL"){		
-			$keyboard = array(
-				'remove_keyboard' => true
-			);
-		}
-		if($keyboard){
-			//Отправка клавиатуры
-			$encodedMarkup = json_encode($keyboard);
-			
-			$data = array(
-				'chat_id'      => $id,
-				'text'     => $message,
-				'parse_mode' => 'HTML',
-				'reply_markup' => $encodedMarkup
-			);
-		}else{
-			//Отправка сообщения
-			$data = array(
-				'chat_id'      => $id,
-				'text'     => $message,
-				'parse_mode' => 'HTML',
-			);
-		}
-       if ($reply_to_message_id != 0) {
-		   $data['reply_to_message_id'] = $reply_to_message_id;
-	   }
-        $out = $this->request('sendMessage', $data);       
-        return $out;
-    }         
-	
-    public function voice($id, $voice,$reply_to_message_id,$keyboard) {   
-		
-		//Удаление клавы
-		if($keyboard == "DEL"){		
-			$keyboard = array(
-				'remove_keyboard' => true
-			);
-		}
-		if($keyboard){
-			//Отправка клавиатуры
-			$encodedMarkup = json_encode($keyboard);
-			
-			$data = array(
-				'chat_id'      => $id,
-				'voice'     => $voice,
-				'reply_markup' => $encodedMarkup
-			);
-		}else{
-			//Отправка сообщения
-			$data = array(
-				'chat_id'      => $id,
-				'file_id'     => $voice,
-			);
-		}
-       if ($reply_to_message_id != 0) {
-		   $data['reply_to_message_id'] = $reply_to_message_id;
-	   }
-        $out = $this->request('sendMessage', $data);       
+
+    public function send($id, $message, $reply_to_message_id, $keyboard)
+    {
+        //Удаление клавы
+        if ($keyboard == "DEL") $keyboard = array('remove_keyboard' => true);
+
+        if ($keyboard) { //Отправка клавиатуры
+            $encodedMarkup = json_encode($keyboard);
+            $data = array(
+                'chat_id' => $id,
+                'text' => $message,
+                'parse_mode' => 'HTML',
+                'reply_markup' => $encodedMarkup
+            );
+        } else { //Отправка сообщения
+            $data = array(
+                'chat_id' => $id,
+                'text' => $message,
+                'parse_mode' => 'HTML',
+            );
+        }
+        if ($reply_to_message_id != 0) $data['reply_to_message_id'] = $reply_to_message_id;
+
+        $out = $this->request('sendMessage', $data);
         return $out;
     }
 
-    public function edit($id, $msg_id, $message,$keyboard) {   
-		
-		//Удаление клавы
-		if($keyboard == "DEL"){		
-			$keyboard = array(
-				'remove_keyboard' => true
-			);
-		}
-		if($keyboard){
-			//Отправка клавиатуры
-			$encodedMarkup = json_encode($keyboard);
-			
-			$data = array(
-				'chat_id'      => $id,
-				'message_id' => $msg_id,
-				'text'     => $message,
-				'reply_markup' => $encodedMarkup
-			);
-		}else{
-			//Отправка сообщения
-			$data = array(
-				'chat_id'      => $id,
-				'message_id' => $msg_id,
-				'text'     => $message
-			);
-		}
-       
-        $out = $this->request('editMessageText', $data);       
+    public function photo($id, $message, $reply_to_message_id, $keyboard)
+    {
+        $photo_message = "\n" . preg_replace('/<.+?>/', '', $message) . "\n";
+        $font_file = './img_font.ttf';
+        $font_size = 10;
+        $img_y = sizeof(explode("\n", $str3)) * $font_size * 1.35;
+        $img_x = 0;
+        $str_arr = preg_split("/\n/", $str3);
+        foreach ($str_arr as $str_val) if ($img_x < strlen($str_val)) $img_x = strlen($str_val);
+        $img_x = $font_size * ($img_x * 0.44 + 2);
+
+        $im = imagecreatetruecolor($img_x, $img_y);
+        $white = imagecolorallocate($im, 0xFF, 0xFF, 0xFF);
+        $black = imagecolorallocate($im, 0x00, 0x00, 0x00);
+
+        imagefilledrectangle($im, 0, 0, $img_x, $img_y, $white);
+        imagefttext($im, $font_size, 0, $font_size, $font_size, $black, $font_file, $photo_message);
+        imagejpeg($im, 'gen_images/c-' . $id . '.jpeg');
+        imagedestroy($im);
+
+        //Удаление клавы
+        if ($keyboard == "DEL") $keyboard = array('remove_keyboard' => true);
+
+        if ($keyboard) { //Отправка клавиатуры
+            $encodedMarkup = json_encode($keyboard);
+            
+            $data = array(
+                'chat_id' => $id,
+                'photo' => 'https://dpos.space/portfoleo-bot/gen_images/c-' . $id . '.jpeg',
+                'caption' => $message,
+                'parse_mode' => 'HTML',
+                'reply_markup' => $encodedMarkup
+            );
+        } else { //Отправка сообщения
+            $data = array(
+                'chat_id' => $id,
+                'photo' => 'https://dpos.space/portfoleo-bot/gen_images/c-' . $id . '.jpeg',
+                'caption' => $message,
+                'parse_mode' => 'HTML',
+            );
+        }
+        if ($reply_to_message_id != 0) $data['reply_to_message_id'] = $reply_to_message_id;
+
+        $out = $this->request('sendPhoto', $data);
         return $out;
     }
-	
-	public function forward($id, $from_chat_id, $msg_id) {   
-		
-			$data = array(
-				'chat_id'      => $id,
-				'from_chat_id' => $from_chat_id,
-				'disable_notification' => false,
-				'message_id' => $msg_id
-			);
-       
-        $out = $this->request('forwardMessage', $data);       
+
+    public function voice($id, $voice, $reply_to_message_id, $keyboard)
+    {
+        //Удаление клавы
+        if ($keyboard == "DEL") $keyboard = array('remove_keyboard' => true);
+
+        if ($keyboard) {//Отправка клавиатуры
+            $encodedMarkup = json_encode($keyboard);
+            $data = array(
+                'chat_id' => $id,
+                'voice' => $voice,
+                'reply_markup' => $encodedMarkup
+            );
+        } else {//Отправка сообщения
+            $data = array(
+                'chat_id' => $id,
+                'file_id' => $voice,
+            );
+        }
+        if ($reply_to_message_id != 0) $data['reply_to_message_id'] = $reply_to_message_id;
+
+        $out = $this->request('sendMessage', $data);
         return $out;
     }
-	
-	public function getPhoto($data){
-		$out = $this->request('getFile', $data);        
+
+    public function edit($id, $msg_id, $message, $keyboard)
+    {
+        //Удаление клавы
+        if ($keyboard == "DEL") $keyboard = array('remove_keyboard' => true);
+
+        if ($keyboard) {//Отправка клавиатуры
+            $encodedMarkup = json_encode($keyboard);
+
+            $data = array(
+                'chat_id' => $id,
+                'message_id' => $msg_id,
+                'text' => $message,
+                'reply_markup' => $encodedMarkup
+            );
+        } else {
+            //Отправка сообщения
+            $data = array(
+                'chat_id' => $id,
+                'message_id' => $msg_id,
+                'text' => $message
+            );
+        }
+
+        $out = $this->request('editMessageText', $data);
         return $out;
-	}  
-	
-	public function savePhoto($url,$puth){
-		$ch = curl_init('https://api.telegram.org/file/bot' . $this->token .  '/' . $url);
-		$fp = fopen($puth, 'wb');
-		curl_setopt($ch, CURLOPT_FILE, $fp);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_exec($ch);
-		curl_close($ch);
-		fclose($fp);
-	}
-	
-    public  function request($method, $data = array()) {
-        $curl = curl_init(); 
-          
-        curl_setopt($curl, CURLOPT_URL, 'https://api.telegram.org/bot' . $this->token .  '/' . $method);
+    }
+
+    public function forward($id, $from_chat_id, $msg_id)
+    {
+        $data = array(
+            'chat_id' => $id,
+            'from_chat_id' => $from_chat_id,
+            'disable_notification' => false,
+            'message_id' => $msg_id
+        );
+
+        $out = $this->request('forwardMessage', $data);
+        return $out;
+    }
+
+    public function getPhoto($data)
+    {
+        $out = $this->request('getFile', $data);
+        return $out;
+    }
+
+    public function savePhoto($url, $puth)
+    {
+        $ch = curl_init('https://api.telegram.org/file/bot' . $this->token . '/' . $url);
+        $fp = fopen($puth, 'wb');
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_exec($ch);
+        curl_close($ch);
+        fclose($fp);
+    }
+
+    public function request($method, $data = array())
+    {
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, 'https://api.telegram.org/bot' . $this->token . '/' . $method);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data); 
-          
-        $out = json_decode(curl_exec($curl), true); 
-          
-        curl_close($curl); 
-          
-        return $out; 
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+        $out = json_decode(curl_exec($curl), true);
+
+        curl_close($curl);
+
+        return $out;
     }
 }
-?>
+
